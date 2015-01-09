f9bf700936c589b37f26bacedcb3158fec94ef74
Daisycrystal <sam.deepthi@gmail.com>
2015-01-08 16:41:41 -0800

    Sum square difference

---
+++ ProjectEuler6.py
@@ -0,0 +1,6 @@
+n=100
+#sum of the squares of first 100 natural numbers = n(n+1)(2n+1)/6
+sum_npow2 = n*(n+1)*(2*n+1)/6
+#square of the sum of the first 100 natural numbers = (n(n+1)/2)**2
+sum_n_pow2 = (n*(n+1)/2)**2
+print sum_n_pow2-sum_npow2
